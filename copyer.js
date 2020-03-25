4a672a16d72c6e4e54dc89d0ff1377e9a2a9a903
Davide Morra <davide@kerkyra.local>
2020-03-25 20:36:20 +0100

    prettier

--- copyer.js
+++ copyer.js
@@ -7,89 +7,75 @@ const {
   collectionsTarget,
   chunks,
   timeOut,
-  action
-} = require("./config");
+  action,
+} = require('./config')
 
-const ACTION_CLONE = "clone";
-const ACTION_APPEND = "append";
-const ACTION_BACKUP_SOURCE = "backup_source";
-const ACTION_DELETE_TARGET = "delete_target";
+const ACTION_CLONE = 'clone'
+const ACTION_APPEND = 'append'
+const ACTION_BACKUP_SOURCE = 'backup_source'
+const ACTION_DELETE_TARGET = 'delete_target'
 
-let fs = require("fs");
-let path = require("path");
-let moment = require("moment");
-let mongoClientSource = require("mongodb").MongoClient;
-let mongoClientTarget = require("mongodb").MongoClient;
+let fs = require('fs')
+let path = require('path')
+let moment = require('moment')
+let mongoClientSource = require('mongodb').MongoClient
+let mongoClientTarget = require('mongodb').MongoClient
 
-const backupDir = path.join(
-  ".",
-  "dump",
-  dbSource,
-  moment().format("YYYYMMDD.HHmmSS")
-);
+const backupDir = path.join('.', 'dump', dbSource, moment().format('YYYYMMDD.HHmmSS'))
 
-console.log("Source Url", urlSource);
-console.log("Source Url", dbSource);
-console.log("Target Url", urlTarget);
-console.log("Target DB", dbTarget);
-console.log("Action", action);
-console.log("-----------------");
+console.log('Source Url', urlSource)
+console.log('Source Url', dbSource)
+console.log('Target Url', urlTarget)
+console.log('Target DB', dbTarget)
+console.log('Action', action)
+console.log('-----------------')
 
 if (action === ACTION_BACKUP_SOURCE && !fs.existsSync(backupDir))
-  fs.mkdirSync(backupDir, { recursive: true });
+  fs.mkdirSync(backupDir, { recursive: true })
 
 for (let i = 0; i < collectionsSource.length; i++) {
-  let collectionSource = collectionsSource[i];
-  let collectionTarget = collectionsTarget[i];
+  let collectionSource = collectionsSource[i]
+  let collectionTarget = collectionsTarget[i]
 
   if (action === ACTION_CLONE || action === ACTION_DELETE_TARGET) {
-    dropCollectionFromTarget(collectionTarget);
+    dropCollectionFromTarget(collectionTarget)
   }
 
   function copyDocumentsInChunks(skip, limit, count) {
     if (action === ACTION_DELETE_TARGET) {
-      return;
+      return
     }
     if (skip >= count) {
-      console.log("Source", collectionSource, "- done");
-      return;
+      console.log('Source', collectionSource, '- done')
+      return
     }
 
     mongoClientSource.connect(
       urlSource,
       { useUnifiedTopology: true, useNewUrlParser: true },
       function(error, mongo) {
-        if (error) throw error;
+        if (error) throw error
 
-        let db = mongo.db(dbSource);
+        let db = mongo.db(dbSource)
         db.collection(collectionSource)
           .find({})
           .sort({ _id: 1 })
           .skip(skip)
           .limit(limit)
           .toArray(function(err, result) {
-            if (err) throw err;
+            if (err) throw err
 
-            if (action === ACTION_BACKUP_SOURCE)
-              backupDocuments(collectionTarget, result);
-            else if (action === ACTION_APPEND)
-              insertDocuments(collectionTarget, result);
-            else if (action === ACTION_CLONE)
-              insertDocuments(collectionTarget, result);
+            if (action === ACTION_BACKUP_SOURCE) backupDocuments(collectionTarget, result)
+            else if (action === ACTION_APPEND) insertDocuments(collectionTarget, result)
+            else if (action === ACTION_CLONE) insertDocuments(collectionTarget, result)
 
-            setTimeout(
-              copyDocumentsInChunks,
-              timeOut,
-              skip + limit,
-              limit,
-              count
-            );
+            setTimeout(copyDocumentsInChunks, timeOut, skip + limit, limit, count)
 
             //copyDocumentsInChunks(skip + limit, limit, count);
-            mongo.close();
-          });
-      }
-    );
+            mongo.close()
+          })
+      },
+    )
   }
 
   function dropCollectionFromTarget(collectionTarget) {
@@ -97,26 +83,21 @@ for (let i = 0; i < collectionsSource.length; i++) {
       urlTarget,
       { useUnifiedTopology: true, useNewUrlParser: true },
       function(error, mongo) {
-        if (error) throw error;
+        if (error) throw error
 
-        let db = mongo.db(dbTarget);
+        let db = mongo.db(dbTarget)
         db.collection(collectionSource)
           .countDocuments()
           .then(count => {
             count >= 0 &&
               db.collection(collectionTarget).drop(function(err, result) {
-                if (err) throw err;
-                if (result)
-                  console.log(
-                    "Target",
-                    collectionTarget,
-                    "- collection deleted"
-                  );
-                mongo.close();
-              });
-          });
-      }
-    );
+                if (err) throw err
+                if (result) console.log('Target', collectionTarget, '- collection deleted')
+                mongo.close()
+              })
+          })
+      },
+    )
   }
 
   function insertDocuments(collectionTarget, documents) {
@@ -124,25 +105,22 @@ for (let i = 0; i < collectionsSource.length; i++) {
       urlTarget,
       { useUnifiedTopology: true, useNewUrlParser: true },
       function(error, mongo) {
-        if (error) throw error;
+        if (error) throw error
 
-        let db = mongo.db(dbTarget);
-        db.collection(collectionTarget).insertMany(documents, function(
-          err,
-          result
-        ) {
-          if (err) throw err;
-          mongo.close();
-        });
-      }
-    );
+        let db = mongo.db(dbTarget)
+        db.collection(collectionTarget).insertMany(documents, function(err, result) {
+          if (err) throw err
+          mongo.close()
+        })
+      },
+    )
   }
 
   function backupDocuments(collectionTarget, documents) {
     fs.appendFileSync(
-      path.join(backupDir, collectionSource + ".json"),
-      JSON.stringify(documents)
-    );
+      path.join(backupDir, collectionSource + '.json'),
+      JSON.stringify(documents),
+    )
   }
 
   function countDocumentsDBSource(callback, limit) {
@@ -150,18 +128,18 @@ for (let i = 0; i < collectionsSource.length; i++) {
       urlSource,
       { useUnifiedTopology: true, useNewUrlParser: true },
       function(error, mongo) {
-        if (error) throw error;
+        if (error) throw error
 
-        let db = mongo.db(dbSource);
+        let db = mongo.db(dbSource)
         db.collection(collectionSource)
           .countDocuments()
           .then(count => {
-            callback(0, limit, count);
-            mongo.close();
-          });
-      }
-    );
+            callback(0, limit, count)
+            mongo.close()
+          })
+      },
+    )
   }
 
-  countDocumentsDBSource(copyDocumentsInChunks, chunks);
+  countDocumentsDBSource(copyDocumentsInChunks, chunks)
 }
