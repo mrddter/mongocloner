08696c3a5d9d61890e77c1c39f69047b387b3d63
TEST <TEST>
2020-08-03 09:03:46 +0200

    add normalize and new version of ongodb

--- processor.js
+++ processor.js
@@ -1,4 +1,5 @@
 const { userInfoRandomizer } = require('./randomizer')
+const mongodb = require('mongodb')
 
 module.exports = {
   process,
@@ -15,20 +16,37 @@ async function initialize(data) {
   applicationStateTypes = await findIds(source, 'applicationstatetype')
 }
 
+function getId(obj) {
+  return mongodb.ObjectID.isValid(obj) ? obj : obj._id
+}
+
+function normalize(values = {}) {
+  const { candidates = [], operations = [], specialist = null, operationInfo = null, owners = [] } = values
+
+  values.specialist = specialist ? getId(specialist) : null
+  values.operationInfo = operationInfo ? getId(operationInfo) : null
+  values.owners = owners.map((user) => (user ? getId(user) : null))
+  values.owners = values.owners.filter((user) => user)
+  values.candidates = candidates.map((can) => ({
+    operation: can.operation ? getId(can.operation) : null,
+    range: can.range,
+  }))
+  values.operations = operations.map((op) => (op ? getId(op) : null))
+  values.operations = values.operations.filter((op) => op)
+
+  return values
+}
+
 async function process(collectionName, documents) {
   if (collectionName === 'application') {
-    documents = await documents.map((application) => {
-      const { state } = application
-      return {
-        ...application,
-        state: state._id ? state : applicationStateTypes[state],
-      }
-    })
-  } else if (collectionName === 'userinfo') {
-    documents = await documents.map((userinfo) => {
-      return userInfoRandomizer(userinfo)
-    })
+    documents = await documents.map((application) => normalize(application))
   }
+
+  // else if (collectionName === 'userinfo') {
+  //   documents = await documents.map((userinfo) => {
+  //     return userInfoRandomizer(userinfo)
+  //   })
+  // }
 
   // do something BUT remember to return documents
   return documents
